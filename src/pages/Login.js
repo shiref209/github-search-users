1715c081341ef607b9ccefa0304f7e36815ce4fa
Sherif Hamam <shiref.hamam2@gmail.com>
2022-06-17 20:45:27 +0200

    delete build

--- src/pages/Login.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Form from "./Form";
-
-const Login =()=>{
-  return <Form name='Login' link='/signup' description='Create new account'/>
-
-}
-
-export default Login;
--- src/pages/SignUp.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Form from "./Form";
-
-const Signup =()=>{
-  return <Form name='Sign up' link='/login' description='I already have an account'/>
-
-}
-
-export default Signup;
